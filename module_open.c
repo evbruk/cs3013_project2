2f1800fe3b81f7c4860929a77df026767506b9b6
Noah Parker <nparker@wpi.edu>
2020-02-05 11:29:51 -0500

    added new function definition to module_open.c

--- module_open.c
+++ module_open.c
@@ -1,3 +1,11 @@
 // module_open.c 
 // ebruk & nparker 
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/syscalls.h>
 
+asmlinkage long new_sys_open(void) 
+{
+	printk(KERN_INFO "***** Intercepted the open function! *****");
+	return 0;
+}
