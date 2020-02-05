89da85f540b35b0420888172464b37b6e0d53104
Eva Bruklich <eva.bruklich@gmail.com>
2020-02-05 11:34:32 -0500

    added arguemtns to sys_open

--- module_open.c
+++ module_open.c
@@ -4,8 +4,11 @@
 #include <linux/module.h>
 #include <linux/syscalls.h>
 
-asmlinkage long new_sys_open(void) 
+asmlinkage long new_sys_open(const char __user *filename,
+				int flags, umode_t mode) 
 {
 	printk(KERN_INFO "***** Intercepted the open function! *****");
 	return 0;
 }
+
+
