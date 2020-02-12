32c5fb3d0bdb2bcc7a06a971c2db5e76f72cecc9
Noah Parker <nparker@wpi.edu>
2020-02-12 11:09:09 -0500

    Adding testing file for part1

--- test.c
+++ test.c
@@ -1,3 +1,4 @@
+//evbruch & nparker
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <errno.h>
@@ -6,45 +7,18 @@
 
 int main(int argc, char * args[]) 
 {
-	printf("Running the system call 1... \n");
-	int rc;
-	rc = syscall(377);
-	if(rc == -1) 
-	{
-		printf("Call failed...\n");
-	}
+	//code to test the kernel output when reading a "malicious" file.
+	FILE* filePointer = fopen("test_document", "r");
+	char * buffer[256];
+	fgets(buffer, 256, filePointer);
+	fclose(filePointer);
 
-	char * buff = "12345zoinks!sksjdlaksjd";
-	char * token = "zoinks!";
-	int malicious = 0;
-	int count = strlen(buff);
-	if( count >= 7 ) 
-	{
-		for(int i = 0; i < count; i++) 
-		{
-			//check against token.
-			char current = buff[i];
-			if (current == 'z')
-			{
-				//don't want to go out of the bounds of the buffer
-				if(i + 7 <= count) 
-				{
-					for(int j = 0; j < 7; j++)
-					{
-						if(buff[i+j] != token[j]) 
-						{
-							break;
-						}
-					}
-					//this should only be reached if a complete 'zoinks!' is found.
-					malicious = 1;
-				}
-			}				
-		}
-	}
+	FILE* negFilePointer = fopen("test_document_nozoinks", "r");
+	fgets(buffer, 256, negFilePointer);
+	fclose(negFilePointer);
 	
-	if(malicious == 1) 
-	{
-		printf("Code is malicious! \n");	
-	}
+//	char * buff = "12345zoinks!sksjdlaksjd";
+//	char * token = "zoinks!";
+		
+	
 }
